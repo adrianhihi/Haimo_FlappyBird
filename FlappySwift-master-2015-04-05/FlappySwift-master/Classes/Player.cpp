b334b77f3068daa923d7cb32e1b9e7140831aa36
adrianhihi <haimobai@usc.edu>
2015-04-05 22:02:00 -0700

    add codes

---
+++ FlappySwift-master-2015-04-05/FlappySwift-master/Classes/Player.cpp
@@ -0,0 +1,100 @@
+#include "Player.h"
+#include "ObjectTag.h"
+#include "cocos2d.h"
+#include "BackgroundLayer_japan.h"
+
+//#include “CocoStudio.h”
+
+//using namespace cocostudio;
+Player::Player()
+{
+	//m_iHP = 100;
+}
+Player::~Player()
+{
+}
+
+void Player::playerInit(float spriteScale)
+{
+	
+}
+
+bool Player::init()
+{
+	isInAir = false;
+	isMovingLeft = false;
+	isLeft = true;
+	isDead = false;
+	score = 0;
+	flyingTime = 0;
+
+	auto size = Director::getInstance()->getWinSize();
+
+	playerWidth = 100;
+
+    m_dir = DIR_LEFT;
+    
+    for (int i = 1; i < 6; i++){
+		auto sf = SpriteFrame::create(StringUtils::format("run/run_left%d.png", i), Rect(0, 0, playerWidth, playerWidth));
+        allframe.pushBack(sf);
+    }
+    
+    Animation * animation = Animation::createWithSpriteFrames(allframe);
+    animation->setDelayPerUnit(0.03);
+    ani = Animate::create(animation);
+    spPlayer = Sprite::create();
+
+    spPlayer->setTag(110);
+    spPlayer->runAction(RepeatForever::create(ani));
+    this->addChild(spPlayer);
+   
+
+
+	return true;
+}
+
+
+
+
+
+void Player::logic()
+{
+//    while (allframe.size() != 0) {
+//        allframe.eraseObject(allframe.at(0));
+//    }
+    
+    for (int i = 0; i < allframe.size(); i++) {
+        allframe.eraseObject(allframe.at(i));
+        i--;
+    }
+    
+    spPlayer->stopAllActions();
+    
+    switch (m_dir) {
+        case DIR_LEFT:
+            for (int i = 1; i < 6; i++){
+				auto sf = SpriteFrame::create(StringUtils::format("run/run_left%d.png", i), Rect(0, 0, 100, 100));
+                allframe.pushBack(sf);
+            }
+            break;
+        
+        case DIR_RIGHT:
+            for (int i = 1; i < 6; i++){
+				auto sf = SpriteFrame::create(StringUtils::format("run/run_right%d.png", i), Rect(0, 0, 100, 100));
+                allframe.pushBack(sf);
+            }
+            break;
+            
+        default:
+            break;
+    }
+    
+    Animation * animation = Animation::createWithSpriteFrames(allframe);
+    animation->setDelayPerUnit(0.03);
+    ani = Animate::create(animation);
+    spPlayer->runAction(RepeatForever::create(ani));
+
+
+}
+
+
---
+++ FlappySwift-master-2015-04-05/FlappySwift-master/Classes/player.h
@@ -0,0 +1,54 @@
+#ifndef Player_H
+#define Player_H
+#include "cocos2d.h"
+#include <sstream>
+
+using namespace std;
+using namespace cocos2d;
+USING_NS_CC;
+
+enum direction{
+    DIR_LEFT,
+    DIR_RIGHT
+};
+class Player : public Node
+{
+public:
+	static Player* createPlayer(const char* fileName, float spriteScale, Vec2 _bgsize, Vec3 _origin);
+
+	Player();
+	~Player();
+	CCSprite* getSprite();
+	void bindSprite(CCSprite* sprite);
+	CREATE_FUNC(Player);
+	virtual bool init();
+
+	int score;
+	bool isInAir;
+	bool isMovingLeft;
+	bool isLeft;
+	bool isDead;
+
+	void logic();
+	void beAtked(int iValue);
+    float playerWidth;
+    direction m_dir;
+
+	Sprite * spPlayer;
+	Vector<SpriteFrame *> allframe;
+	Animate * ani;
+
+private:
+    
+    
+    
+    
+   
+    
+	void playerInit(float spriteScale);
+
+	float flyingTime;
+
+
+};
+#endif
